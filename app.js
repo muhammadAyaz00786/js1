47f9e3b7c59b56e16feaf9d2886df54db1966b9f
1133 <muhammadayaz.znm@gmail.com>
2024-03-20 12:47:49 +0500

    1133

---
+++ app.js
@@ -0,0 +1,37 @@
+var multiArray = [[], [], []];
+
+
+
+var multiArray = [
+  [0, 1, 2, 3],
+  [1, 0, 1, 2],
+  [2, 1, 0, 1],
+];
+document.write(multiArray[0].join(" ") + "<br>");
+document.write(multiArray[1].join(" ") + "<br>");
+document.write(multiArray[2].join(" ") + "<br>");
+
+document.write("<br>");
+document.write("<br>");
+
+
+for (let i = 1; i <=10 ; i++) {
+document.write(i+ "<br>")
+    
+}
+
+var userInput = prompt('enter any number to display its table')
+for (let i = 1; i <=15; i++){
+    document.write(`${userInput} x ${i} = ${userInput*i} <br>`)
+} 
+
+document.write("<br>");
+document.write("<br>");
+
+
+
+
+
+
+
+
